7234221b3785a8b3ab322b0ead45496c4c147973
ITDragonBlog <1625402590@qq.com>
2017-11-25 17:04:57 +0800

    Update JedisClientSingle.java

--- Redis/ssm-redis/src/main/java/com/itdragon/common/utils/JedisClientSingle.java
+++ Redis/ssm-redis/src/main/java/com/itdragon/common/utils/JedisClientSingle.java
@@ -13,7 +13,7 @@ public class JedisClientSingle {
 	 * 1. 检查redis服务是否开启
 	 * 2. 检查是否是因为防火墙的问题
 	 * 3. 检查网络问题(如果在同一个局域网内几乎不会出现这个问题)
-	 * Jedis jedis =new Jedis("localhost",6379,100000);
+	 * Jedis jedis =new Jedis(HOST,PORT,100000);
 	 * JedisPool pool = new JedisPool(poolConfig, HOST, PORT, 100000);
 	 */
 	
