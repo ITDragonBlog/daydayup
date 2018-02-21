0f1526a07a107035113517e485b88057ab076271
ITDragon <1625402590@qq.com>
2018-02-21 16:13:16 +0800

    修改了文件目录

---
+++ ITDragon/EasyUI/EasyUI-tree/ssm-tree/src/main/java/com/itdragon/pojo/Category.java
@@ -0,0 +1,75 @@
+package com.itdragon.pojo;
+
+import java.util.Date;
+
+public class Category {
+    private Integer id;
+
+    private String name;
+
+    private Integer isLeaf;
+
+    private Integer parentId;
+
+    private Date createddate;
+
+    private Date updateddate;
+
+    private Integer status;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Integer getIsLeaf() {
+        return isLeaf;
+    }
+
+    public void setIsLeaf(Integer isLeaf) {
+        this.isLeaf = isLeaf;
+    }
+
+    public Integer getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Integer parentId) {
+        this.parentId = parentId;
+    }
+
+    public Date getCreateddate() {
+        return createddate;
+    }
+
+    public void setCreateddate(Date createddate) {
+        this.createddate = createddate;
+    }
+
+    public Date getUpdateddate() {
+        return updateddate;
+    }
+
+    public void setUpdateddate(Date updateddate) {
+        this.updateddate = updateddate;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+}
