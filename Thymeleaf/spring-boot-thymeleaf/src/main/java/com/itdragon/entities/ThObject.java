dc46e59a773c88731c34c81b1e8d134d152a2436
ITDragonBlog <1625402590@qq.com>
2018-04-08 17:40:45 +0800

    Thymeleaf 变量表达式内置方法

---
+++ Thymeleaf/spring-boot-thymeleaf/src/main/java/com/itdragon/entities/ThObject.java
@@ -0,0 +1,41 @@
+package com.itdragon.entities;
+
+public class ThObject {
+
+    private Long id;
+    private String thName;
+    private String desc;
+
+    public ThObject() {
+    }
+
+    public ThObject(Long id, String thName, String desc) {
+        this.id = id;
+        this.thName = thName;
+        this.desc = desc;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getThName() {
+        return thName;
+    }
+
+    public void setThName(String thName) {
+        this.thName = thName;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
