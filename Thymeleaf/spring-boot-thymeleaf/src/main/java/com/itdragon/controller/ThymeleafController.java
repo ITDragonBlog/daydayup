273f95ff4c4276c04fd863e9a36d4f9d36312651
ITDragonBlog <1625402590@qq.com>
2018-04-11 23:30:25 +0800

    Thymeleaf 实战

--- Thymeleaf/spring-boot-thymeleaf/src/main/java/com/itdragon/controller/ThymeleafController.java
+++ Thymeleaf/spring-boot-thymeleaf/src/main/java/com/itdragon/controller/ThymeleafController.java
@@ -10,9 +10,9 @@ import java.util.*;
 @Controller
 public class ThymeleafController {
 
-    @RequestMapping("dashboard2")
-    public String login() {
-        return "dashboard2";
+    @RequestMapping("dashboard")
+    public String dashboard() {
+        return "dashboard";
     }
 
     @RequestMapping("thymeleaf")
